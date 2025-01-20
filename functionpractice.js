20f9dd93698273c9c225d5a6b98a69b102bbc1ef
sikandershahzad86 <you@example.com>
2025-01-20 17:50:01 +0500

    functionpractice file added

---
+++ functionpractice.js
@@ -0,0 +1,22 @@
+// function number(...num)
+// {
+
+//     return num;
+// }
+
+// console.log(number(2,4,5,6,7,8,9));
+
+
+let car = {
+    name:"Vitz",
+    age:18,
+}
+
+function carDetail(anyobject)
+{
+
+    return `${anyobject.name} and ${anyobject.age}`
+
+}
+
+console.log(carDetail(car));
