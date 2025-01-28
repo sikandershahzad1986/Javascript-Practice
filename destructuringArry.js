8db91b25e6cb610822302f86b57c5aecd4626946
sikandershahzad86 <you@example.com>
2025-01-28 23:34:08 +0500

    New file added

---
+++ destructuringArry.js
@@ -0,0 +1,26 @@
+const people = [
+    { id: 1, name: "John Doe", age:25, phone: "123-456-7890" },
+    { id: 2, name: "Jane Smith", age:30, phone: "987-654-3210" },
+    { id: 3, name: "Alice Johnson", age:28, phone: "555-123-4567" },
+    { id: 4, name: "Bob Brown", age:29,phone: "444-987-6543" },
+  ];
+
+
+let [a,b,c,d] = people;
+  //console.log(people);
+console.log(a);
+
+
+  //
+
+  //const [one,two,three] = stat;
+//   const {name, age} = one;
+//   console.log(stat);
+//   console.log(one);
+//   console.log(name);
+  
+
+  
+  
+  
+  
