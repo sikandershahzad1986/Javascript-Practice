888bfacc30d802d31f43a0bdb8b23993e6557b87
sikandershahzad86 <you@example.com>
2025-02-04 22:14:26 +0500

    New files added

---
+++ destructingArray.js
@@ -0,0 +1,4 @@
+let fruits = ['apple','banana','mango'];
+
+let [first,second,third] = fruits;
+console.log(first);
---
+++ forin.js
@@ -0,0 +1,11 @@
+let ar={
+
+    name:"Vitz",
+    model:2013,
+    color:"White"
+}
+
+for (const key in car) {
+   console.log(key, car[key]);
+   
+}
